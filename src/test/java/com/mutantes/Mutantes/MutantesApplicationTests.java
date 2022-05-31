8fa853ad892090ced7aeca622faea64a1b54a3d7
Nataly Gomez <natagosa@gmail.com>
2022-05-30 19:29:01 -0500

    Creacion de proyecto

---
+++ src/test/java/com/mutantes/Mutantes/MutantesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mutantes.Mutantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
